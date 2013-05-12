7e7fbe7d0e21650f5be8af2a8115f906f1381943
Bruno Batista <bruno@brunobatista.eti.br>
2013-05-12 12:38:57 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-plg_system_scrolltop
-====================
+Plugin System ScrollTop for Joomla! 3.x
+=======================================
 
 Simple plugin to include ScrollTop function.
+
+## Authors
+
+* Bruno Batista
+
+## License
+
+Licensed under the terms of the MIT license.
+
+## Bugs/Requests
+
+* You can [report a bug or request a feature here](http://github.com/joomlapro/plg_system_scrolltop/issues)
---
+++ scrolltop.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.Scrolltop
+ * @copyright   Copyright (C) 2013 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('JPATH_BASE') or die;
+
+/**
+ * Scrolltop plugin.
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  System.Scrolltop
+ * @since       3.1
+ */
+class PlgSystemScrolltop extends JPlugin
+{
+	/**
+	 * Method to catch the onAfterDispatch event.
+	 *
+	 * @return  boolean  True on success
+	 *
+	 * @since   3.1
+	 */
+	public function onAfterDispatch()
+	{
+		// Check that we are in the site application.
+		if (JFactory::getApplication()->isAdmin())
+		{
+			return true;
+		}
+
+		// Get the document object.
+		$doc = JFactory::getDocument();
+
+		// Define path.
+		$path = JUri::root(true) . 'plugins/system/scrolltop';
+
+		// Add Stylesheet.
+		if ($custom_css = $this->params->get('custom_css'))
+		{
+			$doc->addStyleDeclaration($custom_css);
+		}
+		else
+		{
+			$doc->addStyleSheet($path . '/assets/css/template.css');
+		}
+
+		// Add JavaScript Frameworks.
+		JHtml::_('jquery.framework');
+
+		// Build the script.
+		$script = array();
+		$script[] = 'jQuery.noConflict();';
+		$script[] = '(function($) {';
+		$script[] = '	$(function() {';
+		$script[] = '		$("body").append("<a href=\"#\" class=\"scroll-top\">' . $this->params->get('template', '<i class=\"icon-chevron-up\"></i>') . '</a>");';
+		$script[] = '		$(document).ready(function() {';
+		$script[] = '			$(window).scroll(function() {';
+		$script[] = '				if ($(this).scrollTop() > 100) {';
+		$script[] = '					$(".scroll-top").fadeIn();';
+		$script[] = '				} else {';
+		$script[] = '					$(".scroll-top").fadeOut();';
+		$script[] = '				}';
+		$script[] = '			});';
+		$script[] = '			$(".scroll-top").click(function() {';
+		$script[] = '				$("html, body").animate({';
+		$script[] = '					scrollTop: 0';
+		$script[] = '				}, 600);';
+		$script[] = '				return false;';
+		$script[] = '			});';
+		$script[] = '		});';
+		$script[] = '	});';
+		$script[] = '})(jQuery);';
+
+		// Add the script to the document head.
+		$doc->addScriptDeclaration(implode("\n", $script));
+
+		return true;
+	}
+}
